937ad26d13e9322abfad4940c22c49f228412c51
Магомедали Гаджиев <mag198421@gmail.com>
2025-08-10 17:03:46 +0300

    Дабавлены слои проета и включен Swagger

---
+++ src/DirectoryService.Presentation/Controllers/WeatherForecastController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DirectoryService.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    }
+}
