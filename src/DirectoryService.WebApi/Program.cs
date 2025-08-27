57e538edd29dcf4baf5e6455c57605f93f0cd52c
Магомедали Гаджиев <mag198421@gmail.com>
2025-08-27 17:39:09 +0300

    Добавил EndpointResult

--- src/DirectoryService.WebApi/Program.cs
+++ src/DirectoryService.WebApi/Program.cs
@@ -3,12 +3,32 @@ using DirectoryService.Application.Locations.Interfaces;
 using DirectoryService.Infrastructure;
 using DirectoryService.Infrastructure.Repositories;
 using FluentValidation;
+using Microsoft.OpenApi.Models;
+using Shared;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(options =>
+{
+    options.AddSchemaTransformer((schema, context, _) =>
+    {
+        if (context.JsonTypeInfo.Type == typeof(Envelope<Errors>))
+        {
+            if (schema.Properties.TryGetValue("errors", out var errorsProp))
+            {
+                errorsProp.Items.Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.Schema,
+                    Id = "Error"
+                };
+            }
+        }
+
+        return Task.CompletedTask;
+    });
+});
 
 builder.Services.AddScoped<DirectoryServiceDbContext>(_ =>
     new DirectoryServiceDbContext(builder.Configuration.GetConnectionString("DatabaseConnection")!));
