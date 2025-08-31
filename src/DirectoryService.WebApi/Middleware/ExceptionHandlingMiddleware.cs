37b8d495ccab58c2b7b26c8f185ee2d3f09bfa17
Магомедали Гаджиев <mag198421@gmail.com>
2025-09-01 01:24:25 +0300

    Добавил ExceptionHandlingMiddleware

---
+++ src/DirectoryService.WebApi/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Text.Json;
+using Shared;
+
+namespace DirectoryService.WebApi.Middleware;
+
+public class ExceptionHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+    private ILogger<ExceptionHandlingMiddleware> _logger;
+
+    public ExceptionHandlingMiddleware(
+        RequestDelegate next,
+        ILogger<ExceptionHandlingMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+            await _next(httpContext);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An unhandled exception has occurred: {Message}", ex.Message);
+            await HandleExceptionAsync(httpContext, ex);
+        }
+    }
+
+    private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+        var error = Error.Failure("internal.server.error", "An internal server error has occurred.");
+
+        var errors = new Errors(new List<Error> { error });
+        var envelope = Envelope.Error(errors);
+
+        var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        return context.Response.WriteAsJsonAsync(envelope, options);
+    }
+}
